64265127b516fbfd2894f22b62b662b81858fa94
Sébastien Jolly <seb_879@hotmail.fr>
2019-06-04 11:28:50 +0200

    install leaflet and display map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {
+    center: [49.2580045, 4.0317044],
+    zoom: 14,
+  };
+
+  render() {
+    return (
+      <div>
+        <Map center={this.state.center} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png"
+          />
+          <Marker position={this.state.center}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
+    );
+  }
 }
+
 
 export default App;
