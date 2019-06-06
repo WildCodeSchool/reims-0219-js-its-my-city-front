2e75697bb17150275e535d232e83fb27750de416
Greg <gregoire.suray@gmail.com>
2019-06-06 11:03:56 +0200

    first fetch is set for pins positions url

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import AppMap from './Components/AppMap';
 
+import FirstFetch from './Components/FirstFetch';
+
 class App extends Component {
   state = {
     zoom: 14,
     coordonnees: [],
+    poiSampleDisplay: [],
   };
 
 
@@ -13,12 +16,18 @@ class App extends Component {
     navigator.geolocation.getCurrentPosition((position) => {
       this.setState({ coordonnees: [position.coords.latitude, position.coords.longitude] });
     });
+    fetch(URL)
+      .then(response => response.json())
+      .then(responseInJson => this.setState({ poiSampleDisplay: responseInJson.results }));
   }
 
   render() {
-    const { coordonnees, zoom } = this.state;
+    const { coordonnees, zoom, poiSampleDisplay } = this.state;
     return (
       <div>
+        { poiSampleDisplay.map(
+          (currentPinsPosition, index) => <FirstFetch key={index} pins={currentPinsPosition} />,
+        )}
         {coordonnees.length && <AppMap coordonnees={coordonnees} zoom={zoom} />}
       </div>
     );
---
+++ src/Components/FirstFetch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FirstFetch = ({ currentPinsPosition }) => (
+  <div>
+    <p>{currentPinsPosition}</p>
+  </div>
+);
+
+
+export default FirstFetch;
