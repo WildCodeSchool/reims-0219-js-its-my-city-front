3cb6c9cb17961c352a111756ce87546220f0c62e
Chandelier Axel <AxeletCana@hotmail.fr>
2019-06-06 14:26:24 +0200

    now displaying BDD data on the map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import AppMap from './Components/AppMap';
-
-import FirstFetch from './Components/FirstFetch';
 
 class App extends Component {
   state = {
@@ -16,19 +15,16 @@ class App extends Component {
     navigator.geolocation.getCurrentPosition((position) => {
       this.setState({ coordonnees: [position.coords.latitude, position.coords.longitude] });
     });
-    fetch(URL)
-      .then(response => response.json())
-      .then(responseInJson => this.setState({ poiSampleDisplay: responseInJson.results }));
+    axios.get('http://localhost:3001/pois/sample')
+      .then(response => this.setState({ poiSampleDisplay: response.data }))
+      .catch(err => console.log(err));
   }
 
   render() {
     const { coordonnees, zoom, poiSampleDisplay } = this.state;
     return (
       <div>
-        { poiSampleDisplay.map(
-          (currentPinsPosition, index) => <FirstFetch key={index} pins={currentPinsPosition} />,
-        )}
-        {coordonnees.length && <AppMap coordonnees={coordonnees} zoom={zoom} />}
+        {coordonnees.length && <AppMap pins={poiSampleDisplay} coordonnees={coordonnees} zoom={zoom} />}
       </div>
     );
   }
