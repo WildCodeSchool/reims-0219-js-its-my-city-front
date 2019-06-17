05abb6e12e5159c5b4375ac0b0a257dab12c972a
Chandelier Axel <AxeletCana@hotmail.fr>
2019-06-17 17:52:01 +0200

    autosuggestion now working properly

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -7,7 +7,14 @@ const mapStateToProps = state => ({
   searchBarValueInput: state.searchBarValueInput,
 });
 
-const SearchBar = ({ dispatch }) => (
+const keywords = [
+  { name: 'Nature' },
+  { name: 'Sport' },
+];
+
+const filterKeywords = (keyword, userInput) => keyword.filter(el => el.name.toLowerCase().indexOf(userInput.toLowerCase()) !== -1);
+
+const SearchBar = ({ dispatch, searchBarValueInput }) => (
   <div>
     <div className="search-box">
       <input
@@ -16,6 +23,12 @@ const SearchBar = ({ dispatch }) => (
         placeholder="Rechercher"
         onChange={e => dispatch({ type: 'HANDLE_SEARCHBAR_INPUT', searchBarValueInput: e.target.value })}
       />
+      {searchBarValueInput
+      && (
+      <ul>
+        {filterKeywords(keywords, searchBarValueInput).map(word => <li>{word.name}</li>)}
+      </ul>
+      )}
       <Logo className="search-logo" />
     </div>
   </div>
