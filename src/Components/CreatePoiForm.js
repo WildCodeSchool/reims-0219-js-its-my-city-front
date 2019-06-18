a5599e8119e300d3edde744d7fe0173ab24380f3
Matthieu NICOLAS <mtthncls@gmail.com>
2019-06-18 12:04:43 +0200

    adding state and onChange method to the component for controlling + adding onSubmitMethod to be tested

--- src/Components/CreatePoiForm.js
+++ src/Components/CreatePoiForm.js
@@ -1,73 +1,97 @@
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { Component } from 'react';
 import './ComponentsCSS/createPoiForm.css';
 import axios from 'axios';
 
-const onSubmit = (url, data) => {
-  axios.post(url, data)
-    .then(res => console.log(`${res} has been properly sent`))
-    .catch(err => console.log(err));
-};
 
-const CreatePoiForm = () => (
-  // eslint-disable-next-line no-unused-expressions
-  <div>
-    <form className="poi-create" method="POST" encType="multipart/form-data">
+class CreatePoiForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      keywordOne: '',
+      keywordTwo: '',
+    };
+  }
 
-      <legend>Ajoutez un point d'intérêt</legend>
-      <div className="poi-address">
-        <p>
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const url = '';
+    const data = this.state;
+
+    axios.post(url, data)
+      .then(res => console.log(`${res} has been properly sent`))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+    // eslint-disable-next-line no-unused-expressions
+      <div>
+        <form className="poi-create" method="POST" encType="multipart/form-data">
+
+          <legend>Ajoutez un point d'intérêt</legend>
+          <div className="poi-address">
+            <p>
           Adresse:
-          <span> en fonction des coordonnées gps</span>
-        </p>
-      </div>
+              <span> en fonction des coordonnées gps</span>
+            </p>
+          </div>
 
 
-      <div className="poi-name">
-        <label htmlFor="poiName">Nom</label>
-        <br />
-        <input
-          type="text"
-          id="poiName"
-          name="poiName"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="keywordOne">Catégorie:</label>
-        <br />
-        <select id="keywordOne" required>
-          <option value="">Choisissez une catégorie (obligatoire)</option>
-          <option value="nature">Nature</option>
-          <option value="sport">Sport</option>
-          <option value="attraction">Attraction</option>
-          <option value="monument">Monument</option>
-          <option value="utilitaire">Utilitaire</option>
-          <option value="hygiène">Hygiène</option>
-        </select>
-      </div>
+          <div className="poi-name">
+            <label htmlFor="name">Nom</label>
+            <br />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={this.onChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="keywordOne">Catégorie:</label>
+            <br />
+            <select id="keywordOne" name="keywordOne" onChange={this.onChange} required>
+              <option value="">Choisissez une catégorie (obligatoire)</option>
+              <option value="nature">Nature</option>
+              <option value="sport">Sport</option>
+              <option value="attraction">Attraction</option>
+              <option value="monument">Monument</option>
+              <option value="utilitaire">Utilitaire</option>
+              <option value="hygiène">Hygiène</option>
+            </select>
+          </div>
 
-      <div>
-        <label htmlFor="keywordTwo">Sous-catégorie:</label>
-        <br />
-        <select id="keywordTwo">
-          <option value="">Choisissez une sous-catégorie (facultatif)</option>
-          <option value="sports_collectifs">Sports Collectifs</option>
-        </select>
-      </div>
-      <br />
-      <div className="poi-pic">
-        <label htmlFor="poiPic">Ajouter une photo</label>
-        <br />
-        <input id="poiPic" type="file" name="poiPic" />
-      </div>
-      <div className="submit-button">
-        <input type="submit" value="Envoyer" />
-      </div>
+          <div>
+            <label htmlFor="keywordTwo">Sous-catégorie:</label>
+            <br />
+            <select id="keywordTwo" name="keywordTwo" onChange={this.onChange}>
+              <option value="">Choisissez une sous-catégorie (facultatif)</option>
+              <option value="sports_collectifs">Sports Collectifs</option>
+            </select>
+          </div>
+          <br />
+          <div className="poi-pic">
+            <label htmlFor="poiPic">Ajouter une photo</label>
+            <br />
+            <input id="poiPic" type="file" name="poiPic" />
+          </div>
+          <div className="submit-button">
+            <input type="submit" value="Envoyer" />
+          </div>
 
-    </form>
-  </div>
-);
-
+        </form>
+      </div>
+    );
+  }
+}
 export default CreatePoiForm;
