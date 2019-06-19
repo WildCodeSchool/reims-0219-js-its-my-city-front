e352cab9cb63006e0c00e2d3a76bd50e72d3d379
Matthieu NICOLAS <mtthncls@gmail.com>
2019-06-19 10:27:36 +0200

    updating app.js to fit with the modification in the back for the display of the sample pois at loading

--- src/App.js
+++ src/App.js
@@ -17,15 +17,19 @@ const mapStateToProps = state => ({
 class App extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
-    navigator.geolocation.watchPosition((position) => {
+    navigator.geolocation.getCurrentPosition((position) => {
       dispatch({ type: 'GET_CURRENT_POSITION', geolocCoordonnees: [position.coords.latitude, position.coords.longitude] });
     });
-    axios.get('http://localhost:3001/pois/sample')
-      .then(response => dispatch({ type: 'GET_POIS_SAMPLE', poiSampleDisplay: response.data }))
-      .catch(err => console.log(err));
-    axios.get('http://localhost:3001/pois/keywords')
-      .then(response => dispatch({ type: 'GET_POIS_KEYWORDS', poiKeywordsDisplay: response.data }))
-      .catch(err => console.log(err));
+  }
+
+  componentDidUpdate(prevProps) {
+    const { dispatch, geolocCoordonnees } = this.props;
+    if (geolocCoordonnees !== prevProps.geolocCoordonnees) {
+      console.log(geolocCoordonnees);
+      axios.get(`http://localhost:3001/pois/sample/${geolocCoordonnees[0]}/${geolocCoordonnees[1]}`)
+        .then(response => dispatch({ type: 'GET_POIS_SAMPLE', poiSampleDisplay: response.data }))
+        .catch(err => console.log(err));
+    }
   }
 
 
