c3446a357eed1bad1aa206693173cd7c30a6aaff
Chandelier Axel <AxeletCana@hotmail.fr>
2019-06-26 14:46:55 +0200

    The search bar is properly searching for ALL the keywords

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -24,6 +24,10 @@ const dropdownMenuStyle = {
   marginLeft: '-1rem',
 };
 
+const filter = (pois, searchBarInputValue) => pois.filter(
+  poi => poi.importance === 1 && poi.name === searchBarInputValue,
+);
+
 const SearchBar = ({
   dispatch, poiKeywordsDisplay, userInputSearchBar,
 }) => (
@@ -44,10 +48,13 @@ const SearchBar = ({
         )}
         value={userInputSearchBar}
         onChange={e => dispatch({ type: 'HANDLE_SEARCHBAR_INPUT', userInputSearchBar: e.target.value })}
-        onSelect={value3 => axios.get(`${process.env.REACT_APP_API_URL}/pois/filter/${value3}`)
-          .then(res => dispatch({ type: 'HANDLE_SELECT', userInputSearchBar: value3, filteredPoiByKeyword: res.data }))}
+        onSelect={value3 => (filter(poiKeywordsDisplay, value3).length
+          ? axios.get(`${process.env.REACT_APP_API_URL}/pois/filterKeyword1/${value3}`)
+            .then(res => dispatch({ type: 'HANDLE_SELECT', userInputSearchBar: value3, filteredPoiByKeyword: res.data }))
+          : axios.get(`${process.env.REACT_APP_API_URL}/pois/filter/${value3}`)
+            .then(res => dispatch({ type: 'HANDLE_SELECT', userInputSearchBar: value3, filteredPoiByKeyword: res.data })))}
       />
-      <Logo className="search-logo" />
+      <Logo onClick={() => console.log((filter(poiKeywordsDisplay)))} className="search-logo" />
     </div>
   </div>
 );
