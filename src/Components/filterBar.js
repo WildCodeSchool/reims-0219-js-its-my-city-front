f0906766050c9b665836136436b0653c6a060557
Sébastien Jolly <seb_879@hotmail.fr>
2019-06-29 13:13:43 +0200

    correcting typo

--- src/Components/filterBar.js
+++ src/Components/filterBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './ComponentsCSS/filterBar.scss';
+import { ReactComponent as LogoFooterBar } from './pictos/FooterBar.svg';
 import { ReactComponent as LogoProfile } from './pictos/profile.svg';
 import { ReactComponent as LogoFilter } from './pictos/filter.svg';
 import { ReactComponent as LogoAdd } from './pictos/plus-full.svg';
@@ -10,8 +11,11 @@ const mapStateToProps = state => ({
 });
 
 const FilterBar = ({ dispatch }) => (
-  <div>
-    <div className="footer-box">
+  <div className="footer-box">
+    <div className="footer-bar">
+      <LogoFooterBar />
+    </div>
+    <div className="footer-box-button">
       <LogoFilter className="filter-logo" onClick={() => dispatch({ type: 'DISPLAY_FILTER_PAGE' })} />
       <LogoAdd className="add-logo" onClick={() => dispatch({ type: 'TOGGLE_POI_CREATION_FORM' })} />
       <LogoProfile className="profile-logo" />
