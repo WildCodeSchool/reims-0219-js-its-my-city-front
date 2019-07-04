654137aa275b37126ee625c3b500198985d23111
Sébastien Jolly <seb_879@hotmail.fr>
2019-07-04 15:33:31 +0200

    add footerbar

--- src/Components/filterBar.js
+++ src/Components/filterBar.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './ComponentsCSS/filterBar.scss';
-import { ReactComponent as LogoProfile } from './pictos/profile.svg';
-import { ReactComponent as LogoFilter } from './pictos/filter.svg';
-import { ReactComponent as LogoAdd } from './pictos/plus-full.svg';
+import FooterBarPlusSvg from './ComponentPins/FooterBarPlusSvg';
 
-const mapStateToProps = state => ({
-  filterKeywordPageDisplay: state.pois.filterKeywordPageDisplay,
-});
-
-const FilterBar = ({ dispatch }) => (
-  <div>
-    <div className="footer-box">
-      <LogoFilter className="filter-logo" onClick={() => dispatch({ type: 'DISPLAY_FILTER_PAGE' })} />
-      <LogoAdd className="add-logo" onClick={() => dispatch({ type: 'TOGGLE_POI_CREATION_FORM' })} />
-      <LogoProfile className="profile-logo" />
-    </div>
+const FilterBar = () => (
+  <div className="footer-box">
+    <FooterBarPlusSvg />
   </div>
 );
 
-export default connect(mapStateToProps)(FilterBar);
+export default FilterBar;
