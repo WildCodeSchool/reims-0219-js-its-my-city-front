71c6c12140ca101af3a4bbb512334445d6cdb27e
Axel <axelchandelier0@gmail.com>
2019-07-08 15:56:55 +0200

    Checkout

--- src/reducers/poisReducer.js
+++ src/reducers/poisReducer.js
@@ -81,7 +81,7 @@ const poisReducer = (state = initialState, action) => {
     case 'TRANSITION_POI_INFOS':
       return {
         ...state,
-        InformationPoiInfos: action.InformationPoiInfos,
+        InformationPoiInfos: false,
       };
     case 'HANDLE_KEYWORD_FILTERING':
       return {
